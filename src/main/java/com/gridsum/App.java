6b8a0eee26ee335a3c34be373ac40850bff1b50d
林志强 <linclaus@outlook.com>
2018-10-29 18:16:32 +0800

    first commit

---
+++ src/main/java/com/gridsum/App.java
@@ -0,0 +1,13 @@
+package com.gridsum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
